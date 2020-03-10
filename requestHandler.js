4ef2d951044789584401b6991407687cf5e22e78
Lucaspers <dunderdykaren@hotmail.com>
2020-03-10 15:00:56 +0100

    top-list added

--- requestHandler.js
+++ requestHandler.js
@@ -11,8 +11,7 @@ function makeRequest(url, method, FormData, callback) {
     })
 }
 
-getAllUser();
-
+getTopList();
 function getAllUser() {
     makeRequest('./API/Receivers/userReceiver.php?endpoint=getAllUser', 'GET', null, (result) => {
         if (result.status == 404){
@@ -21,3 +20,42 @@ function getAllUser() {
         }
     })
 }
+
+function getTopList() {
+    makeRequest('./API/Receivers/topListReceiver.php?endpoint=getTopList', 'GET', null, (result) => {
+        if (result.status == 404){
+        } else {
+            renderTopList(result);
+        }
+    })
+}
+
+function renderTopList(result) {
+    let MainTopListDiv = document.getElementsByClassName("MainTopListDiv")[0];
+    let TopList = result;
+    
+    let TopListDiv = document.createElement("div");
+    TopListDiv.classList = "TopListDiv";
+    TopListDiv.innerHTML = '';
+    MainTopListDiv.appendChild(TopListDiv);
+
+    for (let i = 0; i < TopList.length; i++) {
+        let selectedTopList = TopList[i];
+        let contentDiv = document.createElement('div');
+        contentDiv.classList = 'contentDiv';
+        
+        let userName = document.createElement('p');
+        userName.classList = 'text';
+        userName.innerText = selectedTopList.userName;
+
+        let point = document.createElement('p')
+        point.classList = 'text';
+        point.innerText = selectedTopList.point + '' + ' Points';
+
+
+        TopListDiv.appendChild(contentDiv);
+
+        contentDiv.appendChild(userName);
+        contentDiv.appendChild(point);
+    }    
+}  
