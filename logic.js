a41b0da4e1506db05a62a5c6fc12afd5099f0df3
Bara Abdullatif <bara.abd@yahoo.com>
2020-03-19 22:25:16 +0100

    Fixa inputfält

--- logic.js
+++ logic.js
@@ -1,51 +1,72 @@
 $(document).ready(function() {
-  var badge = $(".selectedValue");
-  var randomnum = Math.floor(Math.random() * 20);
-  var result = $(".result");
-  console.log(randomnum);
-  $(".userValue").on("change", function() {
-    var current = $(this).val();
-    badge.text(current).animate({
-      left: current + "%"
+    var badge = $(".selectedValue");
+    var randomnum = Math.floor(Math.random() * 20);
+    var result = $(".result");
+    console.log(randomnum);
+
+    $(".userValue").on("change", function() {
+        var current = $(this).val();
+        if (current <= 5) {
+            badge.text(current).animate({
+                left: (current * 45) + 20 + "%"
+            });
+        } else if (current > 5 && current <= 10) {
+            badge.text(current).animate({
+                left: (current * 45) + "%"
+            });
+        } else if (current > 10 && current <= 15) {
+            badge.text(current).animate({
+                left: (current * 45) - 10 + "%"
+            });
+        } else if (current > 15 && current <= 20) {
+            badge.text(current).animate({
+                left: (current * 45) - 20 + "%"
+            });
+
+        }
+
+
+        if (current < randomnum) {
+            console.log("Go up");
+            result.html("Go up");
+        } else if (current > randomnum) {
+            console.log("Go Down");
+            result.html("Go Down");
+        } else {
+            console.log("Guessed");
+            result.html(" ");
+            let img = document.createElement("img");
+            img.src = "img/win.gif";
+            img.setAttribute("alt", "win");
+            img.setAttribute("width", "200px");
+            document.getElementById("resultId").appendChild(img);
+
+            /* result.html("Guessed"); */
+        }
     });
-    if (current < randomnum) {
-      console.log("Go up");
-      result.html("Go up");
-    } else if (current > randomnum) {
-      console.log("Go Down");
-      result.html("Go Down");
-    } else {
-      console.log("Guessed");
-      result.html(" ");
-      let img = document.createElement("img");
-      img.src = "img/win.gif";
-      img.setAttribute("alt", "win");
-      img.setAttribute("width", "200px");
-      document.getElementById("resultId").appendChild(img);
-
-      /* result.html("Guessed"); */
-    }
-  });
 });
+
+
+
 $(document).ready(function() {
-  console.log("test");
-  $("#sidebar").mCustomScrollbar({
-    theme: "minimal"
-  });
+    console.log("test");
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
 
-  $("#dismiss, .overlay").on("click", function() {
-    // hide sidebar
-    $("#sidebar").removeClass("active");
-    // hide overlay
-    $(".overlay").removeClass("active");
-  });
+    $("#dismiss, .overlay").on("click", function() {
+        // hide sidebar
+        $("#sidebar").removeClass("active");
+        // hide overlay
+        $(".overlay").removeClass("active");
+    });
 
-  $("#sidebarCollapse").on("click", function() {
-    // open sidebar
-    $("#sidebar").addClass("active");
-    // fade in the overlay
-    $(".overlay").addClass("active");
-    $(".collapse.in").toggleClass("in");
-    $("a[aria-expanded=true]").attr("aria-expanded", "false");
-  });
+    $("#sidebarCollapse").on("click", function() {
+        // open sidebar
+        $("#sidebar").addClass("active");
+        // fade in the overlay
+        $(".overlay").addClass("active");
+        $(".collapse.in").toggleClass("in");
+        $("a[aria-expanded=true]").attr("aria-expanded", "false");
+    });
 });
