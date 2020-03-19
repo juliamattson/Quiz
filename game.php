9c47ee3a51bdb2f010ce9af049707e6aa6c0d22a
Bara Abdullatif <bara.abd@yahoo.com>
2020-03-19 23:42:37 +0100

    Add Numbers in Input field

--- game.php
+++ game.php
@@ -46,29 +46,29 @@
               
               <form action="">
               <div class="counterContainer">
-              <div  class="number0 number">0</div>
-              <div class="number1 number">1</div>
-              <div  class="number2 number">2</div>
-              <div  class="number3 number">3</div>
-              <div  class="number4 number">4</div>
-              <div  class="number5 number">5</div>
-              <div  class="number6 number">6</div>
-              <div  class="number7 number">7</div>
-              <div  class="number8 number">8</div>
-              <div  class="number9 number">9</div>
-              <div  class="number10 number">10</div>
-              <div  class="number11 number">11</div>
-              <div  class="number12 number">12</div>
-              <div  class="number13 number">13</div>
-              <div  class="number14 number">14</div>
-              <div  class="number15 number">15</div>
-              <div  class="number16 number">16</div>
-              <div  class="number17 number">17</div>
-              <div  class="number18 number">18</div>
-              <div  class="number19 number">19</div>
-              <div  class="number20 number">20</div>               
+              <div  class="number0 number">1</div>
+              <div class="number1 number">2</div>
+              <div  class="number2 number">3</div>
+              <div  class="number3 number">4</div>
+              <div  class="number4 number">5</div>
+              <div  class="number5 number">6</div>
+              <div  class="number6 number">7</div>
+              <div  class="number7 number">8</div>
+              <div  class="number8 number">9</div>
+              <div  class="number9 number">10</div>
+              <div  class="number10 number">11</div>
+              <div  class="number11 number">12</div>
+              <div  class="number12 number">13</div>
+              <div  class="number13 number">14</div>
+              <div  class="number14 number">15</div>
+              <div  class="number15 number">16</div>
+              <div  class="number16 number">17</div>
+              <div  class="number17 number">18</div>
+              <div  class="number18 number">19</div>
+              <div  class="number19 number">20</div>
+              
               </div>
-              <div>
+              
               <input class="userValue" name="amountRange" type="range"  min="0" max="20" step="1" value="0">
               </form>
               <div id="h4-container"><div id="h4-subcontainer"><h4 ><span class="selectedValue">0</span></h4></div></div>
@@ -77,7 +77,7 @@
               <button class="primary-button" type="submit">Submit</button>
               <h4 class="result" id="resultId"></h4>
               </div>
-              </div>
+             
           </article>
         
         </section>
