b10a4b7eea5d34a8651fe2a4f8d6d73ad291d44a
Polina Yaromchyk <pollyyaromchyk@gmail.com>
2020-03-25 15:04:40 +0100

    .

--- logic.js
+++ logic.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     var result = $(".result");
     var power = document.getElementById("power").innerHTML;
         console.log(randomnum);
-    var timeAvailable = 20;
+    var timeAvailable = 30;
     var eleTimer = document.getElementById("timer");
     var timer;
 
@@ -12,6 +12,8 @@ $(document).ready(function() {
         if (timeAvailable < 0) {
             console.log("zero");
             document.getElementById("uValue").disabled = true;
+            document.getElementById("uVal").disabled = true;
+
             result.html("TIME OUT");
             let img2 = document.getElementById("friendly-bot");
             img2.src = "./img/sad.gif";
@@ -22,12 +24,20 @@ $(document).ready(function() {
         }
     }
 
-    $(".userValue").one("change", function() {
+    $(".userValue").one("change", function(e) {
+      e. preventDefault();
       $("#timer").fadeIn();
      timer= setInterval(countdown, 1000);
     });
 
-    $(".userValue").on("change", function() {
+    $("#uVal").one(" click", function(e) {
+      e. preventDefault();
+      $("#timer").fadeIn();
+     timer= setInterval(countdown, 1000);
+    });
+
+    $(".userValue").on("change ", function() {
+     
    
        
             var current = $(this).val();
@@ -93,6 +103,60 @@ $(document).ready(function() {
             }}
        // });
     });
+    
+
+    $("#uVal").on("click", function() {
+     
+   
+       
+            var current = $("#quantity").val();
+            
+
+            power =power- 20;
+            document.getElementById("power").innerHTML = power;
+
+            if (power <= 0) {
+              result.html("GAME OVER");
+                document.getElementById("uVal").disabled = true;
+                let img2 = document.getElementById("friendly-bot");
+                img2.src = "./img/sad.gif";
+                clearInterval(timer);
+            }
+          else{
+            if (current < randomnum) {
+                console.log("Go up");
+                result.html("Go up");
+            } else if (current > randomnum) {
+                console.log("Go Down");
+                result.html("Go Down");
+            } else if (current == randomnum) {
+              result.html("YOU WIN");
+                clearInterval(timer);
+            
+                let img1 = document.getElementById("friendly-bot");
+                img1.src = './img/happy.gif';
+                document.getElementById("uVal").disabled = true;
+
+                $.ajax({
+                    url: "./API/Receivers/resultPointsReceiver.php",
+                    method: "POST",
+                    data: {
+                        power: power
+                    },
+                    dataType: "json",
+                    success: function(response) {
+                        if (response.status == 405) {} else {
+
+                            //top.location = './index.php';
+                        }
+                    }
+                });
+
+                /* result.html("Guessed"); */
+            }}
+       // });
+    });
+
 
     $(document).ready(function() {
         console.log("test");
